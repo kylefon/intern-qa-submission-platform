69af4edb1c0b96b610de5d282fc17b6a11f99b19
TyronVT <tyrontalusan00@gmail.com>
2025-01-30 20:45:00 +0800

    page.tsx h2 test

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default async function Home() {
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Hello World</h2>
+        <h2 className="font-medium text-xl mb-4">This is a new h2 tag.</h2>
       </main>
     </>
   );
