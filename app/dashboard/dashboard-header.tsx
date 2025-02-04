c4218a9fa58e44b7c44f326682de798916547314
unknown <robnekyle@gmail.com>
2025-02-05 04:24:02 +0800

    Add the 'Add Ticket Group' button

--- app/dashboard/dashboard-header.tsx
+++ app/dashboard/dashboard-header.tsx
@@ -1,8 +1,6 @@
 import AddTicketGroupButton from "@/components/add-group-button";
 import { AppTickets } from "@/components/app-tickets";
-import { Button } from "@/components/ui/button";
-import { Tabs, TabsList } from "@/components/ui/tabs";
-import { TabsContent, TabsTrigger } from "@radix-ui/react-tabs";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
 
 export default function DashboardHeader({ appData }: any) {
     return ( 
@@ -10,17 +8,17 @@ export default function DashboardHeader({ appData }: any) {
             <Tabs defaultValue="group" className="w-full space-y-4">
                 <div className="flex gap-2 items-center flex-col justify-between md:flex-row">
                     <TabsList className="w-full grid grid-cols-2 md:w-[50%]">
-                        <TabsTrigger value="group" className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow">Ticket Group</TabsTrigger>
-                        <TabsTrigger value="history" className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow">History</TabsTrigger>
+                        <TabsTrigger value="group">Ticket Group</TabsTrigger>
+                        <TabsTrigger value="history">History</TabsTrigger>
                     </TabsList>
                     <TabsContent value="group" className="w-full md:w-auto bg-green-500 rounded">
-                        <Button className="w-full md:w-auto bg-green-500">Add Ticket Group</Button>
+                        <AddTicketGroupButton />
                     </TabsContent>
                 </div>
                 <TabsContent value="group">
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 auto-cols-fr">
                         {appData.map((data) => (
-                            <AppTickets key={data.id} appName={data.app_name} type={data.type} description={"Ollopa number 1 company"} />
+                            <AppTickets key={data.id} appName={data.app_name} type={data.type} description={data.description} />
                         ))}
                     </div>
                 </TabsContent>
---
+++ components/add-group-button.tsx
@@ -0,0 +1,135 @@
+"use client"
+
+import { z } from "zod";
+import { Input } from "./ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { Textarea } from "./ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { Button } from "./ui/button";
+import { Dialog, DialogFooter, DialogHeader, DialogClose, DialogContent, DialogDescription, DialogTitle, DialogTrigger} from "./ui/dialog";
+import { useForm } from "react-hook-form";
+import { addTicketGroup } from "@/app/ticket-group/[app_name]/actions";
+
+const FormSchema = z.object({
+    app_name: z.string(),
+    type: z.string(),
+    link: z.string(),
+    description: z.string().max(255).optional(),
+})
+
+export default function AddTicketGroupButton() {    
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            app_name: "",
+            type: "",
+            link: "",
+            description: "",
+        },
+    });
+
+    const { control, handleSubmit, formState } = form;
+    const { errors } = formState;
+    
+    async function onSubmit(data: z.infer<typeof FormSchema>) {
+        const { appGroupData, appGroupError } = await addTicketGroup(data);
+
+        if (appGroupError) {
+            console.log("Error adding ticket group: ")
+        }
+
+        if (appGroupData) {
+            console.log("Successfully added ticket group");
+        }
+    }
+    
+    return (
+        <div className="space-y-2">
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button className="w-full md:w-auto bg-green-500">Add Ticket Group</Button>
+                </DialogTrigger>
+                <DialogContent className="max-h-[80vh] overflow-y-auto">
+                    <DialogHeader>
+                        <DialogTitle>Add Ticket Group</DialogTitle>
+                        <DialogDescription>Add ticket groups for interns to submit their tickets</DialogDescription>
+                    </DialogHeader>
+                    <Form {...form}>
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-2">
+                            <FormField 
+                                control={control}
+                                name="app_name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter ticket group name" {...field}/>
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField 
+                                control={control}
+                                name="type"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Event Type</FormLabel>
+                                        <Select
+                                            onValueChange={field.onChange}
+                                            defaultValue={field.value}
+                                        >
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Select app type"/>
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem value="APK">APK</SelectItem>
+                                                <SelectItem value="WEB">WEB</SelectItem>
+                                                <SelectItem value="OTHER">Other</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField 
+                                control={control}
+                                name="link"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Link</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter link" {...field}/>
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField 
+                                control={control}
+                                name="description"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Description</FormLabel>
+                                        <FormControl>
+                                            <Textarea {...field}/>
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+
+                            <DialogFooter className="mt-2">
+                                <DialogClose asChild>
+                                    <Button>Cancel</Button>
+                                </DialogClose>
+                                <Button type="submit">Submit</Button>
+                            </DialogFooter>
+                        </form>
+                    </Form>
+                </DialogContent>
+            </Dialog>
+        </div>
+    )
+}
+
+
